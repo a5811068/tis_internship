8cf38ab9944696a6061f2466b4cd9500518e6970
a5811068 <a5811068@aoyama.jp>
2014-02-27 16:47:41 +0900

    no3

---
+++ no3/no3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void){
+  int i,n;
+  
+  printf("n=");
+  scanf("%d",&n);
+  
+  for(i=n;i>0;i--){
+    
+    
+  return 0;
+}
+
+void generate_paren(int n){
+  int 
+}
+
+void add_paren(){
+  printf();
+}
